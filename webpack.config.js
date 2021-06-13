e579c250431bbc1daf9923a3e50862fdcfb84e2c
Ruslan <rhgalyamov@gmail.com>
2021-06-14 00:04:58 +0300

    edited project structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ let path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './js/script.js',
+  entry: './src/js/script.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/js'
+    path: __dirname + '/dist/js'
   },
   watch: true,
 
